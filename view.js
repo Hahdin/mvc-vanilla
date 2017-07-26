6715632c7e20e89d5809c5197beb63df21e084e1
Mark Foster <mark.foster@abracontrols.local>
2017-07-26 13:51:46 -0600

    abstraction

--- view.js
+++ view.js
@@ -5,21 +5,36 @@ var view = function view(element){
 }
 
 view.prototype.render = function render(viewModel){
-    this.element.innerHTML = '<h3>' + viewModel.name + '</h3>' +
-    '<img class="item-image" src="' + viewModel.imageUrl +
-      '" alt="' + viewModel.name + '" />' +
-    '<p><b>Size:</b> ' + viewModel.size + '</p>' +
-    '<p><b>Favorite food:</b> ' + viewModel.favoriteFood + '</p>' +
-    '<a id="previousItem" class="previous button" href="javascript:void(0);"' +
-      ' data-item-index="' + viewModel.previousIndex + '">Previous</a> ' +
-    '<a id="nextItem" class="next button" href="javascript:void(0);"' +
-      ' data-item-index="' + viewModel.nextIndex + '">Next</a>'
 
-      this.previousIndex = viewModel.previousIndex
-      this.nextIndex = viewModel.nextIndex
-      var previousItem = this.element.querySelector('#previousItem')
-      previousItem.addEventListener('click', this.onClickGetItem)
-      var nextItem = this.element.querySelector('#nextItem')
-      nextItem.addEventListener('click', this.onClickGetItem)
+  let keys = Object.keys(viewModel)
+  let vals = Object.values(viewModel)
+  //name, image url, data 1, data 2, prev index, next index
+  console.log(keys, vals)
+  this.element.innerHTML = '<h3>' + viewModel[keys[0]] + '</h3>' +
+    '<img class="item-image" src="' + viewModel[keys[1]] +
+    '" alt="' + viewModel[keys[0]] + '" />'  +
+    '<p><b>'+ keys[2] +':</b> ' + viewModel[keys[2]] + '</p>' +
+    '<p><b>' + keys[3] +':</b> ' + viewModel[keys[3]] + '</p>' +
+    '<a id="previousItem" class="previous button" href="javascript:void(0);"' +
+    ' data-item-index="' + viewModel[keys[4]] + '">Previous</a> ' +
+    '<a id="nextItem" class="next button" href="javascript:void(0);"' +
+    ' data-item-index="' + viewModel[keys[5]] + '">Next</a>'
+
+    // '<img class="item-image" src="' + viewModel.imageUrl +
+    // '" alt="' + viewModel.name + '" />'  +
+    // '<p><b>Size:</b> ' + viewModel.size + '</p>' +
+    // '<p><b>Favorite food:</b> ' + viewModel.favoriteFood + '</p>' +
+    // '<a id="previousItem" class="previous button" href="javascript:void(0);"' +
+    // ' data-item-index="' + viewModel.previousIndex + '">Previous</a> ' +
+    // '<a id="nextItem" class="next button" href="javascript:void(0);"' +
+    // ' data-item-index="' + viewModel.nextIndex + '">Next</a>'
+
+
+  this.previousIndex = viewModel.previousIndex
+  this.nextIndex = viewModel.nextIndex
+  var previousItem = this.element.querySelector('#previousItem')
+  previousItem.addEventListener('click', this.onClickGetItem)
+  var nextItem = this.element.querySelector('#nextItem')
+  nextItem.addEventListener('click', this.onClickGetItem)
 
 }
