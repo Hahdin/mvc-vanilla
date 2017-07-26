fe9343d66b8a6fa442a29376d4b1e122d4a3c2ae
Mark Foster <mark.foster@abracontrols.local>
2017-07-26 13:58:06 -0600

    abstraction

--- view.js
+++ view.js
@@ -20,16 +20,6 @@ view.prototype.render = function render(viewModel){
     '<a id="nextItem" class="next button" href="javascript:void(0);"' +
     ' data-item-index="' + viewModel[keys[5]] + '">Next</a>'
 
-    // '<img class="item-image" src="' + viewModel.imageUrl +
-    // '" alt="' + viewModel.name + '" />'  +
-    // '<p><b>Size:</b> ' + viewModel.size + '</p>' +
-    // '<p><b>Favorite food:</b> ' + viewModel.favoriteFood + '</p>' +
-    // '<a id="previousItem" class="previous button" href="javascript:void(0);"' +
-    // ' data-item-index="' + viewModel.previousIndex + '">Previous</a> ' +
-    // '<a id="nextItem" class="next button" href="javascript:void(0);"' +
-    // ' data-item-index="' + viewModel.nextIndex + '">Next</a>'
-
-
   this.previousIndex = viewModel.previousIndex
   this.nextIndex = viewModel.nextIndex
   var previousItem = this.element.querySelector('#previousItem')
